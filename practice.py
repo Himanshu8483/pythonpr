c72d80b1c50259168b90953213dfd9c9788c0109
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-01-22 20:42:12 +0530

    all

--- practice.py
+++ practice.py
@@ -12,3 +12,21 @@ print(type(z))      # float
 x=10+3j
 y=20.01+3j
 print(type(x/y))        # complex
+
+# String (no character in python one char is also string)
+x='Him'
+y="ANSHU"
+print(x+y)
+print(type(x+y))        # str
+# Inbuilt Function : print(), input(), max(), min, id , type, len
+x='python'
+print(x)
+# x=input("Enter Name : ")
+# print(x)
+print(type(x))
+print(id(x))
+print(type('10'))       # str
+print(max(x))           # y
+print(len(x))           # 6
+print(x.index('y'))     # 1
+# Function(direct call) and Method(collection.method) difference 
